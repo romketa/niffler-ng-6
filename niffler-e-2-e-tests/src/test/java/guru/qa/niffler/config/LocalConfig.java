f169d190d8d52ce739286c659fae11f8c79067aa
dtuchs <dtuchs@gmail.com>
2024-08-31 23:12:20 +0500

    stream #6 - JDBC: intro
    # Conflicts:
    #	niffler-e-2-e-tests/src/test/java/guru/qa/niffler/config/Config.java
    #	niffler-e-2-e-tests/src/test/java/guru/qa/niffler/config/LocalConfig.java

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/config/LocalConfig.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/config/LocalConfig.java
@@ -39,6 +39,11 @@ enum LocalConfig implements Config {
   }
 
   @Override
+  public String authJdbcUrl() {
+    return "jdbc:postgresql://127.0.0.1:5432/niffler-auth";
+  }
+
+  @Override
   public String gatewayUrl() {
     return "http://127.0.0.1:8090/";
   }
@@ -46,5 +51,25 @@ enum LocalConfig implements Config {
   @Override
   public String userdataUrl() {
     return "http://127.0.0.1:8089/";
+  }
+
+  @Override
+  public String userdataJdbcUrl() {
+    return "jdbc:postgresql://127.0.0.1:5432/niffler-userdata";
+  }
+
+  @Override
+  public String spendUrl() {
+    return "http://127.0.0.1:8093/";
+  }
+
+  @Override
+  public String spendJdbcUrl() {
+    return "jdbc:postgresql://127.0.0.1:5432/niffler-spend";
+  }
+
+  @Override
+  public String currencyJdbcUrl() {
+    return "jdbc:postgresql://127.0.0.1:5432/niffler-currency";
   }
 }
