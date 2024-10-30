53551cdd21e1290f49cabbfa241b7078603e781a
Dmitrii Tuchs <dtuchs@gmail.com>
2024-10-30 15:19:35 +0500

    stream #17 - allure lifecycle (#54)
    # Conflicts:
    #	niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingWebTest.java
    #	niffler-e-2-e-tests/src/test/java/utils/ScreenDiffResult.java

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingWebTest.java
@@ -1,6 +1,8 @@
 package guru.qa.niffler.test.web;
 
 import com.codeborne.selenide.Selenide;
+import guru.qa.niffler.jupiter.annotation.ScreenShotTest;
+import guru.qa.niffler.jupiter.annotation.Spending;
 import guru.qa.niffler.config.Config;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.annotation.meta.WebTest;
@@ -9,8 +11,18 @@ import guru.qa.niffler.model.SpendJson;
 import guru.qa.niffler.model.UserJson;
 import guru.qa.niffler.page.LoginPage;
 import guru.qa.niffler.page.MainPage;
+import guru.qa.niffler.utils.RandomDataUtils;
+import guru.qa.niffler.utils.ScreenDiffResult;
+import org.junit.jupiter.api.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.Date;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @WebTest
 public class SpendingWebTest {
@@ -71,6 +83,27 @@ public class SpendingWebTest {
         .save();
 
     new MainPage().checkThatTableContainsSpending("Category name");
+  }
+
+
+  @User(
+      spendings = @Spending(
+          category = "Обучение",
+          description = "Обучение Advanced 2.0",
+          amount = 79990
+      )
+  )
+  @ScreenShotTest("img/expected-stat.png")
+  void checkStatComponentTest(UserJson user, BufferedImage expected) throws IOException {
+    Selenide.open(LoginPage.URL, LoginPage.class)
+        .fillLoginPage(user.username(), user.testData().password())
+        .submit(new MainPage());
+
+    BufferedImage actual = ImageIO.read($("canvas[role='img']").screenshot());
+    assertFalse(new ScreenDiffResult(
+        expected,
+        actual
+    ));
   }
 }
 
