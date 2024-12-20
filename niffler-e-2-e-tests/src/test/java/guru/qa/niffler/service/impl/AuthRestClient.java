248f2116ee55f1aa6bdbbea6f8eb61b32f6e7dbc
romketa <mr@romketa.ru>
2024-12-20 21:26:11 +0400

    HW

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/impl/AuthRestClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/impl/AuthRestClient.java
@@ -14,25 +14,15 @@ public class AuthRestClient extends RestClient {
 
   private static final Config CFG = Config.getInstance();
   private final AuthApiClient authApi = new AuthApiClient();
-  private final AuthApi authApi2;
 
   public AuthRestClient() {
     super(CFG.authUrl(), true, new CodeInterceptor());
-    this.authApi2 = create(AuthApi.class);
   }
 
   @SneakyThrows
   public String login(String username, String password) {
     final String codeVerifier = OAuthUtils.generateCodeVerifier();
     final String codeChallenge = OAuthUtils.generateCodeChallenge(codeVerifier);
-    authApi2.authorize(
-        "code",
-        "clientId",
-        "openid",
-        CFG.frontUrl() + "authorized",
-        codeChallenge,
-        "S256"
-    ).execute();
     authApi.authorize(codeChallenge);
     authApi.login(username, password);
     return authApi.token(ApiLoginExtension.getCode(), codeVerifier);
